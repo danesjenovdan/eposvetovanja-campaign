b75b1347329981c452201a3e85320de227f08e19
LepkoQQ <lepko.san@gmail.com>
2020-06-16 19:26:28 +0200

    fix logos and add links

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,8 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+  },
   plugins: [],
 };
