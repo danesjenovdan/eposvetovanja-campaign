55dfe94592af3f88bc69b59427e45e7aaf0a87c3
LepkoQQ <lepko.san@gmail.com>
2020-06-16 20:47:49 +0200

    party og images and set correct up/down icons for parties

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ function ogFromTemplate(party) {
   return {
     title: `Uvažava li ${party} naš glas`,
     description: `Iz ${party} još nisu potvrdili da će uvažavati glas građanki i građana nakon izbora. Tražimo pravovremenu najavu, primjereno trajanje i argumentirane odgovore vlasti na komentare pristigle na javnom savjetovanju. Poručite ${party} da imamo pravo sudjelovati u donošenju odluka koje nas se tiču.`,
+    og_image: `{ TODO: base url }og/${party.toLowerCase()}.jpg`,
   };
 }
 
@@ -18,16 +19,17 @@ const ogData = {
     title: 'Uvažite naš glas i između izbora',
     description:
       'Savjetovanje s javnošću jednostavan je i svakom građaninu_ki dostupan mehanizam sudjelovanja u odlukama vlasti. Provođenje javnog savjetovanja pokazuje i koliko vlasti uvažavaju mišljenje svojih građanki i građana. Više od 20 posto zakonskih propisa donesenih u mandatu IX. saziva Hrvatskog sabora nije prošlo savjetovanje s javnošću.',
+    og_image: `{ TODO: base url }og/_default.jpg`,
   },
-  hdz: ogFromTemplate('HDZ'),
-  sdp: ogFromTemplate('SDP'),
-  most: ogFromTemplate('MOST'),
-  glas: ogFromTemplate('GLAS'),
-  ids: ogFromTemplate('IDS'),
-  hns: ogFromTemplate('HNS'),
   band: ogFromTemplate('BANDIĆ MILAN'),
+  glas: ogFromTemplate('GLAS'),
+  hdz: ogFromTemplate('HDZ'),
+  // hns: ogFromTemplate('HNS'),
   hss: ogFromTemplate('HSS'),
+  ids: ogFromTemplate('IDS'),
+  most: ogFromTemplate('MOST'),
   nlm: ogFromTemplate('NLM'),
+  sdp: ogFromTemplate('SDP'),
   sdss: ogFromTemplate('SDSS'),
 };
 
